e2710ea8e17bf1c125f1b645a6206dfdac8ac6f2
KaiKaiiiii <90381850+KaiKaiiiii@users.noreply.github.com>
2022-06-01 11:50:04 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00A7B4",
+        secondary: "#A4D96C",
+      },
+    },
+  },
+  plugins: [],
+};
